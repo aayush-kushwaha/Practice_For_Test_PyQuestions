752b565a56136d75bb7b032b5582cf94ce0adef6
Aayush Kushwaha <1728107@kiit.ac.in>
2022-07-22 19:47:52 +0530

    second_problem_solved

--- WHILE_LOOP/print_n_natural_number.py
+++ WHILE_LOOP/print_n_natural_number.py
@@ -1,10 +1,14 @@
-a = int(input("Enter Number: "))
-# Incrementatio Order
+a = int(input("Enter Number: ")) # Asking inout from user
+# Incrementation Order
 i = 1 # Initialising a looping variable
-while i < a: # if i < a then the loop runs
+while i <= a: # if i < a then the loop runs
     print(i) # Printing i
     i += 1 # Updation of looping variable. If it is not done then it will stuck in infinite loop
-print(i)
-# while a > 0:
-#     print(a)
-#     a -= 1
+
+
+#Decrementation Order
+'''
+while a > 0:
+    print(a)
+    a -= 1
+'''
