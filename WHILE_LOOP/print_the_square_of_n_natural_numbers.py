9817e49c2f4837abd93e8b2824a5fc25aa46312b
Aayush Kushwaha <1728107@kiit.ac.in>
2022-07-22 20:12:17 +0530

    5th_Question_solved

---
+++ WHILE_LOOP/print_the_square_of_n_natural_numbers.py
@@ -0,0 +1,6 @@
+# WAP_to_print_the_square_of_n_natural_numbers
+n = int(input("Enter number of series till where you want the square of number: ")) # Taking input from user
+i = 1 # Initialising a looping variable
+while i <= n: # if i is less than or equals to n then the loop runs
+    print(i) # Printing i till loop runs
+    i += 1 # Updation of looping variable. If it is not done then it will stuck in infinite loop
