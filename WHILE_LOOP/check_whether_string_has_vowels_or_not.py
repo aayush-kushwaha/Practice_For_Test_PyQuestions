9575c09ed3052dd9a044bdd3f892e19109f6833d
Aayush Kushwaha <1728107@kiit.ac.in>
2022-07-22 19:51:13 +0530

    added_comment

--- WHILE_LOOP/check_whether_string_has_vowels_or_not.py
+++ WHILE_LOOP/check_whether_string_has_vowels_or_not.py
@@ -1,9 +1,9 @@
 # WAP_to_check_whether_the_string_has_vowels_or_not
-a = input("Enter String: ")
-i = 0
-while i < len(a):
-    if a[i] in "AEIOUaeiou":
-        print(a[i], "is a Vowel")
+a = input("Enter String: ") # Asking input from user
+i = 0 # Initialising a looping variable
+while i < len(a): # if i < len(a) then the loop runs
+    if a[i] in "AEIOUaeiou": # checking the character of string index by index. If any index value is in "AEIOUaeiou" then it will be a vowel
+        print(a[i], "is a Vowel") # Printing a[i] index + "is a Vowel"
     else:
-        print(a[i],"is not a Vowel")
-    i = i +1
+        print(a[i],"is not a Vowel") # Printing a[i] index + "is not a Vowel
+    i = i + 1 # Updation of looping variable. If it is not done then it will stuck in infinite loop
