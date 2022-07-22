3d6ead9a3cd487e5d10c15d478819a602dee0b12
Aayush Kushwaha <1728107@kiit.ac.in>
2022-07-22 19:59:44 +0530

    3rd_Question_solved

---
+++ WHILE_LOOP/program_to_get_the_following_output.py
@@ -0,0 +1,13 @@
+'''
+WAP to get the following output:
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+'''
+m = 5 # Taking variable with value 5
+i = 1 # Initialising a looping variable
+while i <= m: # If i is less than or equals to m (i.e. 5) then the loop runs 
+    print("5","*",i,"=",m*i) # Here, print("5","*",i,"=",m*i) means printing 5 * i = m*i
+    i += 1 # Updation of looping variable. If it is not done then it will stuck in infinite loop
